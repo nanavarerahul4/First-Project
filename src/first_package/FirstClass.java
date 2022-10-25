8590275342f5a7fab9d4001e584a1b51eb83a0fa
Admin <Admin@hp>
2022-10-25 19:59:11 +0530

    first project 1

---
+++ src/first_package/FirstClass.java
@@ -0,0 +1,12 @@
+package first_package;
+
+public class FirstClass
+{
+    public static void main(String [] args)
+    {
+    	System.out.println("Om Nam Shivay");
+    	System.out.println("Ganpati Bappa Morya");
+    	System.out.println("Om Sha Shaneshwaray Namah");
+    	System.out.println("Jotibachya Navan Changbhal");
+    }
+}
